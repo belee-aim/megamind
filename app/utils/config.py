e20c388b8d30b9078ddf3322933b7fbdc7518dfc
skele2k <tsatsralt@aim.mn>
2025-06-10 14:52:16 +0800

    feat: supabase env variables

--- app/utils/config.py
+++ app/utils/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     frappe_url: str
     frappe_api_key: str
     frappe_api_secret: str
+    supabase_url: str
+    supabase_key: str
+    supabase_connection_string: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
